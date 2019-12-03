c3a010ea8d3cbd64e647278e8e84ce5ac04b456c
dvirp <dvirp@directeam.io>
2019-12-03 23:50:20 +0200

    init

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messegeOne =document.querySelector('#messege-1')
+const messegeTwo =document.querySelector('#messege-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const loction = search.value
+
+    messegeOne.textContent = 'Searching...'
+    messegeTwo.textContent = ''
+    fetch('http://localhost:3000/weather?address=' + loction).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            messegeOne.textContent = data.error
+        } else {
+            messegeOne.textContent = data.location
+            messegeTwo.textContent = data.forecast
+        }
+    })
+})
+
+})
